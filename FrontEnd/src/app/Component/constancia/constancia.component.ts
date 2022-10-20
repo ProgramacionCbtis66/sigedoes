16276b3897b34c21306ea3321a81b8042f3239f4
MSC-JorgeCortes <108251042+MSC-JorgeCortes@users.noreply.github.com>
2022-10-20 17:35:11 -0500

    Agregue el modulo constancia
    Agregue el modulo constancia

---
+++ FrontEnd/src/app/Component/constancia/constancia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-constancia',
+  templateUrl: './constancia.component.html',
+  styleUrls: ['./constancia.component.css']
+})
+export class ConstanciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
