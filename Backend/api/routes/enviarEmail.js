6309dc99fe5a4059aafdfd6739dd97fc08b3858f
MSC-JorgeCortes <108251042+MSC-JorgeCortes@users.noreply.github.com>
2022-10-22 09:14:27 -0500

    Backend Prueba de Correo
    Backend Prueba de Correo

--- Backend/api/routes/enviarEmail.js
+++ Backend/api/routes/enviarEmail.js
@@ -2,19 +2,41 @@ const express = require('express');
 var bodyParser = require('body-parser')
 const Emailrouter = express.Router();
 var nodemailer = require('nodemailer')
+var email = "";
 
-Emailrouter.post('/enviar', (req, res)=>{
-    const email = req.body;
-    EnviarCorreo()
+
+Emailrouter.post('/enviar', (req, res) => {
+    
+    email = req.body;
+    console.log(email);
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "jorgecortescbtis66@gmail.com",
+        pass: "ibwchrluddbkxjea"
+      }
+    });
+
+    const mailOptions = {
+      from: `"Control escolar", "jorgecortescbtis66@gmail.com"`,
+      to: `"${email.email}"`,
+      subject: `"${email.asunto}"`,
+      html: "<h1>Prueba de correo enviado</h1>"
+    };
+   
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+        res.send('error') // if error occurs send error as response to client
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.send('Sent Successfully')//if mail is sent successfully send Sent successfully as response
+      }
+    });
 });
+   
 
-const EnviarCorreo = (mail, callback) => {
-        const transporter = nodemailer.createTransport({
-          host: "smtp.gmail.com",
-          port: 587,
-          secure: false,
-          auth: {
-            user: "<sender email>",
-            pass: "<password>"
-          }
-}
+module.exports = Emailrouter;
