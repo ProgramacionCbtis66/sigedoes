e861a51b216d251944fb6ccc7ff6e41d700e90b2
MSC-JorgeCortes <108251042+MSC-JorgeCortes@users.noreply.github.com>
2022-10-27 16:33:43 -0500

    a
    a

--- Backend/api/connection/connection.js
+++ Backend/api/connection/connection.js
@@ -4,11 +4,8 @@ const ccn = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-
-    password : 'juanitoc5',
-    database:'alumno',
-    password : '1205',
-    database:'proyecto'
+    database:'constancias',
+    password : '1205'
 });
 
 ccn.connect (err=>{
