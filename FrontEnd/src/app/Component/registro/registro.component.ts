0f3543b85856a4de3409bb1056d17cf00f3c564e
JuanPedrocbtis66 <112534901+JuanPedrocbtis66@users.noreply.github.com>
2022-10-27 18:36:01 -0500

    .

--- FrontEnd/src/app/Component/registro/registro.component.ts
+++ FrontEnd/src/app/Component/registro/registro.component.ts
@@ -31,68 +31,33 @@ export class RegistroComponent implements OnInit {
 
   constructor(private auth: AuthService,  private router: Router, private app : AppComponent) { app.registro=true; app.iflogin=false;}
 
-  ngOnInit(): void {
-
-  }
-  verificar(){
-
-  }
+  ngOnInit(): void {}
   Registro(){
-    let contra = "";
-    let contra2 = "";
-    let correo = "";
-    let noUsuario = "";
-    contra = this.usuario.pass;
-    contra2 = this.usuario.pass2;
-    correo = this.usuario.correo;
-    noUsuario = this.usuario.UserName;
-
-    if(contra != "" && contra2 != "" && correo != "" && noUsuario != ""){
-
-      if(contra == contra2){
-        Notiflix.Notify.info("Las contraseñas coinciden");
-        Notiflix.Loading.standard("Guardando Datos");
-        console.log (this.usuario);
-      }else{
-          Notiflix.Notify.failure("Las Contraseñas No Coinciden");
+   
+    if (this.usuario.correo !== "" && this.usuario.pass !== "" && this.usuario.pass2 !== "" && this.usuario.UserName !== "" && this.usuario.curp !== "" && this.usuario.noctrl !== ""  && this.usuario.especialidad !== ""  && this.usuario.semestre !== "" && this.usuario.area !== "" && this.usuario.turno !== "")
+    {
+      Notiflix.Loading.standard("Accesando");
+      this.auth.login(this.usuario).subscribe((res: any) => 
+      {
+        if (res.token !== null && res.token != undefined) 
+        {
+          localStorage.setItem('color', res.token);
+          this.app.visibleLoginRegistro();
+          Notiflix.Loading.remove();
+          this.router.navigate(['/home']);
+        } else if (res.Error == "Usuario y contraseña incorrecta") 
+        {
+          Notiflix.Loading.remove();
+          Notiflix.Notify.warning("Usuario y contraseña incorrecta");
+        } else 
+        {
+          Notiflix.Loading.remove();
+          Notiflix.Notify.failure("Error de conexion, intente mas tarde");
         }
-    }else{
-      Notiflix.Notify.warning("Debe De Llenar Todos Los Campos")
+      });
+    } else {
+      Notiflix.Notify.failure("Usuario o contraseña vacio, llene los campos");
     }
-    }
-
-    public contraseña() {
-      Notiflix.Loading.standard("Ingresando");
-    let contra = "";
-    let contra2 = "";
-    contra = this.usuario.pass;
-    contra2 = this.usuario.pass2;
-    if(contra == contra2){
-      Notiflix.Notify.info("Las contraseñas coinciden");
-    }else{
-      Notiflix.Notify.failure("Las Contraseñas No Coinciden");
-    }
-  
-
-  if (this.usuario.correo !== "" && this.usuario.pass !== ""){
-    Notiflix.Loading.standard("Accesando");
-    this.auth.login(this.usuario).subscribe((res: any) => {
-      if (res.token !== null && res.token != undefined) {
-        localStorage.setItem('color', res.token);
-        this.app.visibleLoginRegistro();
-        Notiflix.Loading.remove();
-        this.router.navigate(['/home']);
-      } else if (res.Error == "Usuario y contraseña incorrecta") {
-        Notiflix.Loading.remove();
-        Notiflix.Notify.warning("Usuario y contraseña incorrecta");
-      } else {
-        Notiflix.Loading.remove();
-        Notiflix.Notify.failure("Error de conexion, intente mas tarde");
-      }
-    });
-  } else {
-    Notiflix.Notify.failure("Usuario o contraseña vacio, llene los campos");
   }
-}
 }
   
