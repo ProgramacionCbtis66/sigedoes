75e0de87509f66f28bd7580a54a2ddd2de4beff5
JuanPedrocbtis66 <112534901+JuanPedrocbtis66@users.noreply.github.com>
2022-11-03 14:43:38 -0600

    Revert "Registro funcional"
    This reverts commit 279f33d44ffe451805101068730f7ae5d4da8f62.

--- Backend/api/routes/user.js
+++ Backend/api/routes/user.js
@@ -1,4 +1,3 @@
-const { dir } = require('console');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
@@ -60,32 +59,20 @@ router.post('/login', (req, res) => {
     const {correo,pass,pass2,curp,noctrl,especialidad,semestre,area,turno,nombre,direccion} = req.body;
     ccn.query('INSERT INTO usuario (idUser,userName,numControl,password,rol,exp,nombre) VALUES (?, ?, ?, ?, ?, ?, ?, )',[noctrl,nombre,])
 });*/
-
-router.post('/registro',(req,res)=>{
-    const {nombre,correo,pass,pass2,curp,noctrl,especialidad,semestre,area,turno,direccion,CTO,grupo} = req.body;
-    const horario = "si";
-    const alta = 0; 
-    const rol = "user";
-        //Subir datos a la tabla usuario
-    ccn.query('INSERT INTO usuario (userName, numControl, password, rol, exp, nombre) VALUES (?, ?, ?, ?, ?, ?)',[noctrl,noctrl,pass2,rol, 300, nombre],(err,rows,fields)=>{
-        if(!err){
-            console.log(rows.affectedRows);
-        }
-    });
-
-        //Subir los datos a la tabla Alumno
-    ccn.query('INSERT INTO alumno (numControl,nombre,direccion,especialidad,area,grado,grupo,turno,horario,CTO,correo,alta,CURP) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [noctrl,nombre,direccion,especialidad,area,semestre,grupo,turno,horario,CTO,correo,alta,curp],
-    
+/*router.post('/registro',(req,res)=>{
+    const {correo,pass,pass2,curp,noctrl,especialidad,semestre,area,turno,nombre,direccion} = req.body;
+    let xd = "3";
+    let horario = "si";
+    let CTO = "30DCT0236O";
+    let alta = "1";
+    console.log(req.body);
+    ccn.query('INSERT INTO alumno (numControl,nombre,direccion,especialidad,area,grado,grupo,turno,horario,CTO,correo,alta,CURP,) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [numcontrol,nombre,direccion,especialidad,area,semestre,xd,turno,horario,CTO,correo,alta,curp],
     (err,rows,fields)=>{
         if(!err){
-            if(rows.affectedRows > 0){
-                res.json({Aceptado:"Datos Aceptados"});
-            }else{
-                res.json({Error:"Los Datos No Fueron Aceptados"});
-            }
+            console.log("correcto");
         }
     });
-});
+});*/
 
 router.post('/datosUser', (req, res) => {
     const numControls = req.body.numcontrol;
--- FrontEnd/src/app/Component/registro/registro.component.ts
+++ FrontEnd/src/app/Component/registro/registro.component.ts
@@ -17,6 +17,11 @@ export class RegistroComponent implements OnInit {
   informacion = 'Info';
   infografia : string = '.././assets/img/infografiaa.png';
   usuario = {
+    "username":"",
+    "numControl":"",
+    "password":"",
+    "rol":"",
+    "exp":300,
     "nombre":"",
     "correo": "",
     "pass": "",
@@ -27,26 +32,8 @@ export class RegistroComponent implements OnInit {
     "semestre":"",
     "area":"",
     "turno":"",
-    "direccion":"",
-    "CTO":"30DCT0236O",
-    "grupo":""
-  }
-  datanecesario = {
-    "numControl":"", 
-    "nombre" :"",
-    "direccion" : "",
-    "especialidad": "",
-    "area":"",
-    "grado":"",
-   // "grupo":"",
-    "turno":"",
-   // "horario":"",
-    "CTO":"",
-    "correo":"",
-    "alta":"0",
-    "CURP":"",
-            
-  }
+    "direccion":""
+  };
 
   constructor(private auth: AuthService,  private router: Router, private app : AppComponent) { app.registro=true; app.iflogin=false;}
 
@@ -55,19 +42,16 @@ export class RegistroComponent implements OnInit {
    let contra = this.usuario.pass2;
    let contra2 = this.usuario.pass;
     if(contra == contra2){
-    if ( this.usuario.correo !== "" && this.usuario.pass !== "" && this.usuario.pass2 !== ""  && this.usuario.curp !== "" && this.usuario.noctrl !== "" && this.usuario.especialidad !== ""  && this.usuario.semestre !== "" && this.usuario.area !== "" && this.usuario.turno !== "" && this.usuario.grupo != "")
+    if ( this.usuario.correo !== "" && this.usuario.pass !== "" && this.usuario.pass2 !== ""  && this.usuario.curp !== "" && this.usuario.noctrl !== "" && this.usuario.especialidad !== ""  && this.usuario.semestre !== "" && this.usuario.area !== "" && this.usuario.turno !== "")
     {
-      Notiflix.Loading.standard("Validando");
+      Notiflix.Loading.standard(this.usuario.especialidad);
       this.auth.registro(this.usuario).subscribe((res: any) => 
       {
-        if (res.Aceptado == "Datos Aceptados") 
+        if (res.token !== null && res.token != undefined) 
         {
           Notiflix.Loading.remove();
           this.router.navigate(['/login']);
-        }else if(res.Error == "Los Datos No Fueron Aceptados"){
-          Notiflix.Loading.remove();
-          Notiflix.Notify.failure(res.Error);
-        }
+        } 
       });
     } else {
       Notiflix.Notify.failure("Por favor llene todos los campos");
