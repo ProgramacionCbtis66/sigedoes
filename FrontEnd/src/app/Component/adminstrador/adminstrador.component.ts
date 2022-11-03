a9ce542cff41d04cb0664c9abbd2982e4a6a1be2
MSC-JorgeCortes <108251042+MSC-JorgeCortes@users.noreply.github.com>
2022-11-03 17:22:05 -0600

    .

--- FrontEnd/src/app/Component/adminstrador/adminstrador.component.ts
+++ FrontEnd/src/app/Component/adminstrador/adminstrador.component.ts
@@ -104,7 +104,7 @@ export class AdminstradorComponent implements OnInit {
 
   generateRandomString(num: number) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$#@&';
-    let result1 = ' ';
+    let result1 = '';
     const charactersLength = characters.length;
     for (let i = 0; i < num; i++) {
       result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
