8086d8664d266e3b6673b392668e51cfd035c425
FelixHdzz <114973155+FelixHdzz@users.noreply.github.com>
2022-11-07 11:37:51 -0600

    pie de formato

--- Backend/api/routes/pdfCreate.js
+++ Backend/api/routes/pdfCreate.js
@@ -53,7 +53,10 @@ function create(req, tipo) {
                             },
 
                             { text: " \n Director", style: Style.header },
-                            { text: `${alumno.Esc_Director}`, style: Style.firma, alignment: "center" }
+                            { text: `${alumno.Esc_Director}`, style: Style.firma, alignment: "center" },
+
+                            {text: "15 de septiembre Esq. J.R. Claveria, Col. Hoja del Maiz, Tierra Blanca, Ver. C.P.95110 Tel. 274 74 3 09 62 correco eléctronico: cbtis066dir@dgeti.sems.gob.mx"},
+                            {image:'./api/assets/pieformato.jpg', width:100}
                         ]
                     };
 
