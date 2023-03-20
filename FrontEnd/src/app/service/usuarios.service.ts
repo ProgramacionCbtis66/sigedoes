3774745c4857a13c029fe9e9647082bd79659754
MSC-JorgeCortes <108251042+MSC-JorgeCortes@users.noreply.github.com>
2023-03-20 03:16:43 -0600

    Observables en los servicios 20-03-23

--- FrontEnd/src/app/service/usuarios.service.ts
+++ FrontEnd/src/app/service/usuarios.service.ts
@@ -10,77 +10,77 @@ import { environment } from 'src/environments/environment';
 })
 export class UsuarioService {
   private URL = environment.HTTPS;
-   
-
-  constructor(private http:HttpClient) { }
 
 
-  forgotPassword(email:any):Observable<any>{
-    return this.http.post(`${this.URL}/insize/forgotPassword`,email);
+  constructor(private http: HttpClient) { }
+
+
+  forgotPassword(email: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/forgotPassword`, email);
   }
 
-  datosUser(numcontrol:any):Observable<any>{
-    return this.http.post(`${this.URL}/insize/datosUser`,numcontrol);
+  datosUser(numcontrol: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/datosUser`, numcontrol);
   }
 
-  usuarioAceptado(numcontrol:any):Observable<any>{
-      return this.http.post(`${this.URL}/insize/usuarioAceptado`,numcontrol);
+  usuarioAceptado(numcontrol: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/usuarioAceptado`, numcontrol);
   }
 
-  UsuariosNoReg():Observable<any>{
+  UsuariosNoReg(): Observable<any> {
     return this.http.get(`${this.URL}/insize/listaUserNoReg`);
   }
 
-NoPago(datos:any):Observable<any>{
-    return this.http.post(`${this.URL}/insize/NoPago`,datos);
-}
-verificaNoPago(datos:any):Observable<any>{
-  return this.http.post(`${this.URL}/insize/verificaNoPago`,datos);
-}
-NoPagoDesactivo(noPago:any):Observable<any>{
-  return this.http.post(`${this.URL}/insize/NoPagoDesactivo`,noPago);
-}
+  NoPago(datos: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/NoPago`, datos);
+  }
+  verificaNoPago(datos: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/verificaNoPago`, datos);
+  }
+  NoPagoDesactivo(noPago: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/NoPagoDesactivo`, noPago);
+  }
 
-obtenerDatos(nopago:any):Observable<any>{
-  return this.http.post(`${this.URL}/insize/ObtenerDatosPago`,nopago);
-}
-subirEmitido(datosRegistro:any):Observable<any>{
-  return this.http.post(`${this.URL}/insize/SubirRegistro`,datosRegistro);
-}
-verInfo(numControl:any):Observable<any>{
-  return this.http.post(`${this.URL}/insize/verInfo`,numControl);
-}
-getContra(numControl:any):Observable<any>{
-  return this.http.post(`${this.URL}/insize/getContra`,numControl);
-}
-modificarPerfil(datos:any):Observable<any>{
-  return this.http.post(`${this.URL}/insize/modifyProfile`,datos);
-}
-datosEsc():Observable<any>{
-  return this.http.get(`${this.URL}/insize/GetdatosEsc`);
-}
-guardarDatosEsc(escuela:any){
-  return this.http.post(`${this.URL}/insize/guardarDatosEsc`,escuela);
-}
-guardarClavesEsp(claves:any){
-  return this.http.post(`${this.URL}/insize/guardarClavesEsp`,claves);
-}
-optenerClavesEsp(){
-  return this.http.get(`${this.URL}/insize/optenerClavesEsp`);
-}
-guardarClavesEspProg(prog:any){
-  return this.http.post(`${this.URL}/insize/guardarClavesEspProg`,prog);
-}
-guardarClavesEspconta(conta:any){
-  return this.http.post(`${this.URL}/insize/guardarClavesEspconta`,conta);
-}
-guardarClavesEspElectricidad(electricidad:any){
-  return this.http.post(`${this.URL}/insize/guardarClavesEspElectricidad`,electricidad);
-}
-guardarClavesEspAlimentos(alimentos:any){
-  return this.http.post(`${this.URL}/insize/guardarClavesEspAlimentos`,alimentos);
-}
-guardarClavesEspSoporte(soporte:any){
-  return this.http.post(`${this.URL}/insize/guardarClavesEspSoporte`,soporte);
-}
+  obtenerDatos(nopago: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/ObtenerDatosPago`, nopago);
+  }
+  subirEmitido(datosRegistro: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/SubirRegistro`, datosRegistro);
+  }
+  verInfo(numControl: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/verInfo`, numControl);
+  }
+  getContra(numControl: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/getContra`, numControl);
+  }
+  modificarPerfil(datos: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/modifyProfile`, datos);
+  }
+  datosEsc(): Observable<any> {
+    return this.http.get(`${this.URL}/insize/GetdatosEsc`);
+  }
+  guardarDatosEsc(escuela: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/guardarDatosEsc`, escuela);
+  }
+  guardarClavesEsp(claves: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/guardarClavesEsp`, claves);
+  }
+  optenerClavesEsp(): Observable<any> {
+    return this.http.get(`${this.URL}/insize/optenerClavesEsp`);
+  }
+  guardarClavesEspProg(prog: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/guardarClavesEspProg`, prog);
+  }
+  guardarClavesEspconta(conta: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/guardarClavesEspconta`, conta);
+  }
+  guardarClavesEspElectricidad(electricidad: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/guardarClavesEspElectricidad`, electricidad);
+  }
+  guardarClavesEspAlimentos(alimentos: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/guardarClavesEspAlimentos`, alimentos);
+  }
+  guardarClavesEspSoporte(soporte: any): Observable<any> {
+    return this.http.post(`${this.URL}/insize/guardarClavesEspSoporte`, soporte);
+  }
 }
