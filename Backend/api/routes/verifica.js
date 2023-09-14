4c3584248e7d41491eedaa723476b2944b9ce498
Alexa-Acevedo-Pavon <112530435+Alexa-Acevedo-Pavon@users.noreply.github.com>
2023-09-14 15:50:50 -0600

    Update verifica.js

--- Backend/api/routes/verifica.js
+++ Backend/api/routes/verifica.js
@@ -1,22 +1,15 @@
-usuario.post('/usr/login',async (req, res) => {
-    const {usuario,password} = req.body;
-    var registro = '';
-    const sql = 'SELECT nomComp as usrname, rol, id_usr as id FROM usuario WHERE nombre = ? AND pass=?';
-    const conexion = await con();
-    try{
-        const [record] = await conexcion.execute(sql,[usuario,password]);
-        registro = record;
-    }catch(error){
-        console.log(error);
-    }finally{
-        conexion.end();
-    }
-    if (registro.length > 0){
-        var user = JSON.stringify(registro[0]);
-        var usr = JSON.parse(usr);
-        const token =jwt.sign(usr,'ñ↓N', {expiresIn: '30m'});
-        res.json({token});
-    }else{
-        res.json({mensaje: 'Usuario o contraseña incorrectos'});
-    }
-});
+const verifica = require('jsonwebtoken').verify;
+
+function verificaToken(req, res, next) {
+    const cabecera = req.headers.authorization;
+    const token = cabecera && cabecera.split(' ')[1];
+
+    verifica(token, 'MA@L', { ignoreExpiration: true }, (error, usuario) => {
+        if (error) return res.sendStatus(403);
+        const horaActual = Date.now() / 1000;
+        if (usuario.exp < horaActual) return res.sendStatus(401);
+        next();
+    });
+}
+
+module.exports = verificaToken;
