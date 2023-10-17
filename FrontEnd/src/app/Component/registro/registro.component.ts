89465a7ce0944f2e4fb0a83e5c5c48d754132cce
Ingrid Samanta Durate Gil <duarteingrid102@gmail.com>
2023-10-17 11:26:27 -0600

    optimizando el base64 como servicio

--- FrontEnd/src/app/Component/registro/registro.component.ts
+++ FrontEnd/src/app/Component/registro/registro.component.ts
@@ -7,6 +7,7 @@ import * as Notiflix from 'notiflix';
 import { environment } from 'src/environments/environment';
 import { NavegacionService } from 'src/app/service/navegacion.service';
 import { DomSanitizer } from '@angular/platform-browser';
+import { UsuarioService } from 'src/app/service/usuarios.service';
 
 
 
@@ -30,7 +31,8 @@ export class RegistroComponent implements OnInit {
   constructor(private auth: AuthService, 
     private router: Router, 
     private nav: NavegacionService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private Base64 :UsuarioService
     ) { this.nav._logout= false;this.nav._registro=true; this.nav._iflogin=false; }
 
   ngOnInit(): void {}
@@ -88,11 +90,11 @@ export class RegistroComponent implements OnInit {
 
   cargarFoto(event: any): void {
     const archivo = event.target.files[0];
-    this.extraerBase64(archivo).then((imagenBase64: any) => {
+    this.Base64.extraerBase64(archivo).then((imagenBase64: any) => {
 
       const foto64 = imagenBase64.base;
 
-      this.redimensionarImagen(foto64, 110, 110).then((imagenRedimensionada: any) => {
+      this.Base64.redimensionarImagen(foto64, 110, 110).then((imagenRedimensionada: any) => {
         this.foto = imagenRedimensionada.base;
       }).catch((error: any) => {
         console.error('Error al redimensionar la imagen', error);
@@ -105,53 +107,5 @@ export class RegistroComponent implements OnInit {
   activarInput() {
     this.fileInput.nativeElement.click();
   }
-
-  extraerBase64 = async (foto: any) => new Promise((resolve, reject) => {
-    try {
-      const unsafeImg = window.URL.createObjectURL(foto);
-      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
-      const reader = new FileReader();
-      reader.readAsDataURL(foto);
-      reader.onload = () => {
-        resolve({
-          base: reader.result
-        });
-      };
-      reader.onerror = error => {
-        reject('Error al leer la imagen');
-      };
-    } catch (e) {
-      reject('Error inesperado al procesar la imagen'); // Devolver un valor de error en caso de excepción
-    }
-  });
-
-  redimensionarImagen = (foto: any, anchoDeseado: number, altoDeseado: number) => new Promise((resolve, reject) => {
-    try {
-      const img = new Image();
-  
-      img.onload = () => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-        canvas.width = anchoDeseado;
-        canvas.height = altoDeseado;
-        ctx.drawImage(img, 0, 0, anchoDeseado, altoDeseado);
-        const imagenRedimensionadaBase64 = canvas.toDataURL('image/jpeg'); // Cambiar el formato si es necesario
-        resolve({ base: imagenRedimensionadaBase64 });
-      };
-  
-      img.onerror = () => {
-        console.error('Error al cargar la imagen');
-        reject('Error al cargar la imagen');
-      };
-  
-      img.src = foto; // Ya tenemos la imagen en formato base64 aquí, no es necesario crear un objeto URL
-    } catch (e) {
-      console.error('Error inesperado al procesar la imagen');
-      reject('Error inesperado al procesar la imagen');
-    }
-  });
-  
-  
-  
 }
 
