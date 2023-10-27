22dc3498398c80dba002f6bb0e898aad46f1e3b7
Ingrid Samanta Durate Gil <duarteingrid102@gmail.com>
2023-10-26 20:09:43 -0600

    Update environment.ts

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -7,7 +7,7 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: false,
-  HTTPS: "http://localhost:8000",
+  HTTPS: "https://backendsigedoes.onrender.com",
   autorization: {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
