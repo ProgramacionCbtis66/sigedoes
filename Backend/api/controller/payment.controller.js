d7950907bf95b5d0b326357fb180831f45411b61
Ingrid Samanta Durate Gil <duarteingrid102@gmail.com>
2023-11-08 23:23:03 -0600

    yupiiiiiiiiiiiiiiii

--- Backend/api/controller/payment.controller.js
+++ Backend/api/controller/payment.controller.js
@@ -1,7 +1,7 @@
 const mercadopago = require('mercadopago');
-const {ACCESS_TOKEN, NOTIFICACION_URL,PORT,HOST} = require('../../config.js');
+const { ACCESS_TOKEN, NOTIFICACION_URL, PORT, HOST } = require('../../config.js');
 
- const createOrden = async (req,res) => {
+const createOrden = async (req, res) => {
     const item = req.body;
     console.log(item);
     /*const item = [
@@ -26,60 +26,61 @@ const {ACCESS_TOKEN, NOTIFICACION_URL,PORT,HOST} = require('../../config.js');
     ];*/
 
     mercadopago.configure({
-          access_token: ACCESS_TOKEN,
+        access_token: ACCESS_TOKEN,
         // lo de arriba es indicador de donde va el api de mercado pago cuenta prueba
     });
     const results = await mercadopago.preferences.create({
         items: [
-           item
+            item
         ],
-        back_urls:{
-            success: HOST+PORT+"/pagos/success",
-            failure: HOST+PORT+"/pagos/failure",
-            pending: HOST+PORT+"/pagos/pending",
-         },
-         notification_url: NOTIFICACION_URL+"/pagos/webhook",
+        back_urls: {
+            success: HOST + PORT + "/pagos/success",
+            failure: HOST + PORT + "/pagos/failure",
+            pending: HOST + PORT + "/pagos/pending",
+        },
+        notification_url: NOTIFICACION_URL + "/pagos/webhook",
     });
-     
-     res.json({web:results.body.init_point})
-    };
-    const receiveWebhook = async (req, res) => {
 
-       const payment = req.query;
-       console.log(payment);
-        try {
-            if(payment.type === 'payment'){
-                const data = await mercadopago.payment.findById(payment['data.id']);
-                 
-                const datos =[
-                    {
-                        id: data.body.id,
-                        status: data.body.status,
-                        detalleStatus: data.body.status_detail,
-                        description: data.body.description,
-                        monto: data.body.transaction_amount,
-                        comisionMercadoPago: data.body.transaction_details.total_paid_amount - data.body.transaction_details.net_received_amount,
-                        total: data.body.transaction_details.net_received_amount,
-                        correo: data.body.payer.email,
-                        urlNotificacion: data.body.notification_url,
-                        fecha_aprobacion: data.body.date_approved,
-                        external_reference: data.body.external_reference,
-                        payment_type: data.body.payment_type_id,
-                        merchant_order_id: data.body.order.id
-                    }]
-                
-                console.log(datos);
-            }
-            res.status(204).json({datos});
-        } catch (error) {
-            console.log(error);
-            res.status(500).send('error',error);
+    res.json({ web: results.body.init_point })
+};
+
+const receiveWebhook = async (req, res) => {
+
+    const payment = req.query;
+    console.log(payment);
+    try {
+        if (payment.type === 'payment') {
+            const data = await mercadopago.payment.findById(payment['data.id']);
+
+            const datos = [
+                {
+                    id: data.body.id,
+                    status: data.body.status,
+                    detalleStatus: data.body.status_detail,
+                    description: data.body.description,
+                    monto: data.body.transaction_amount,
+                    comisionMercadoPago: data.body.transaction_details.total_paid_amount - data.body.transaction_details.net_received_amount,
+                    total: data.body.transaction_details.net_received_amount,
+                    correo: data.body.payer.email,
+                    urlNotificacion: data.body.notification_url,
+                    fecha_aprobacion: data.body.date_approved,
+                    external_reference: data.body.external_reference,
+                    payment_type: data.body.payment_type_id,
+                    merchant_order_id: data.body.order.id
+                }]
+
+            console.log(datos);
         }
+        res.status(204).json({ msj:"ok" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('error', error);
+    }
 }
 
-const pendiente = async (req,res) => {
+const pendiente = async (req, res) => {
     console.log(req.body);
     res.send('pendiente');
 }
 
-module.exports = {createOrden, receiveWebhook, pendiente};
+module.exports = { createOrden, receiveWebhook, pendiente };
