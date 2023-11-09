a764a8a363b7e089016a9842472be9ad80a46838
Ingrid Samanta Durate Gil <duarteingrid102@gmail.com>
2023-11-09 08:06:29 -0600

    ssss

--- Backend/config.js
+++ Backend/config.js
@@ -6,5 +6,6 @@ const HOST = process.env.HOST || "http://localhost:";
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const NOTIFICACION_URL = process.env.NOTIFICACION_URL;
+ 
 
 module.exports = { ACCESS_TOKEN, PUBLIC_KEY, NOTIFICACION_URL, PORT, HOST };
