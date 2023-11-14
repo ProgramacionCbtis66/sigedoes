f931b5ffb7e945eb071fb1177d505c847b192146
dochtml <88597899+dochtml@users.noreply.github.com>
2023-11-14 17:54:15 -0600

    justificante

--- FrontEnd/src/app/service/justificante.service.ts
+++ FrontEnd/src/app/service/justificante.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,5 +10,13 @@ export class JustificanteService {
   private URL = environment.HTTPS;
   private usr = environment.autorization;
   
-  constructor(){}
+  constructor(private http: HttpClient){}
+
+  datosJustificantes(datosJust: any): Observable<any>{
+    return this.http.post(`${URL}/just/obtenerdatos`, datosJust, this.usr);
+  }
+
+  guardarJustificante(guardarJust: any): Observable<any>{
+    return this.http.post(`${URL}/just/guardardatos`, guardarJust, this.usr);
+  }
 }
