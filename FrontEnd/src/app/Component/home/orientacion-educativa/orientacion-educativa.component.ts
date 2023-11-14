88c2efa5375c214758ca18c8d4de6a95f147a1c9
dochtml <88597899+dochtml@users.noreply.github.com>
2023-11-14 16:24:23 -0600

    new service & justificante

--- FrontEnd/src/app/Component/home/orientacion-educativa/orientacion-educativa.component.ts
+++ FrontEnd/src/app/Component/home/orientacion-educativa/orientacion-educativa.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/service/auth.service';
 import { NavegacionService } from 'src/app/service/navegacion.service';
+import { UsuarioService } from 'src/app/service/usuarios.service';
 
 @Component({
   selector: 'app-orientacion-educativa',
@@ -8,13 +9,17 @@ import { NavegacionService } from 'src/app/service/navegacion.service';
   styleUrls: ['./orientacion-educativa.component.css']
 })
 export class OrientacionEducativaComponent implements OnInit {
+  protected nombre:string = "administrativo";
+  protected justificantes: any;
 
   constructor(
     private nav: NavegacionService,
-    private auth: AuthService
+    private auth: AuthService,
+    //private alumno: Justifi,
   ) { 
     this.nav._usuario = this.auth.decodifica().nombre+ " " + this.auth.decodifica().apellidoP + " " + this.auth.decodifica().apellidoM;
     this.nav._foto = this.auth.decodifica().foto;
+    this.nav._orientacionEdu = true;
   }
 
   ngOnInit(): void {
