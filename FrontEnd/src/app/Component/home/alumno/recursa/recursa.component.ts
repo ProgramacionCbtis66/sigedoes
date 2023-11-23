ab621ebaa5e193f1d37d547c8df57bb63629bf31
dochtml <88597899+dochtml@users.noreply.github.com>
2023-11-23 13:07:54 -0600

    .

--- FrontEnd/src/app/Component/home/alumno/recursa/recursa.component.ts
+++ FrontEnd/src/app/Component/home/alumno/recursa/recursa.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
+import { NavegacionService } from 'src/app/service/navegacion.service';
+import { RecursaService } from 'src/app/service/recursa.service';
+import { UsuarioService } from 'src/app/service/usuarios.service';
 
 @Component({
   selector: 'app-recursa',
@@ -6,8 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recursa.component.css']
 })
 export class RecursaComponent implements OnInit {
+  protected recursaDatos = {
+    alumnoNumControl: '',
+    idMateria: '',
+    idPeriodo: '',
+    docenteDni: '',
+    fecha: '',
+    estado: 2,
+    docenteDniApli: '',
+  }
 
-  constructor() { }
+  constructor(
+    private alumno: UsuarioService,
+    private recursa: RecursaService,
+    private auth: AuthService,
+    private nav: NavegacionService,
+  ){
+    this.nav._usuario = this.auth.decodifica().nombre+ " " + this.auth.decodifica().apellidoP + " " + this.auth.decodifica().apellidoM;
+    this.nav._foto = this.auth.decodifica().foto;
+    this.nav._recursa = true;
+  }
 
   ngOnInit(): void {
   }
